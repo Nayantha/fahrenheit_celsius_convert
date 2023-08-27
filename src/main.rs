38597eb8745a2bcf56348775a7a5f35fd25e9cda
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-27 15:10:04 +0530

    add conversion method logic combined with getting io input.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
+use std::io;
+
 fn main() {
     println!("Welcome to Fahrenheit, Celsius Converter....");
     println!("What would you like to convert from and to?");
     println!("1. Fahrenheit to Celsius\n2. Celsius to Fahrenheit");
+    // choose conversion method input
+    let mut conversion_method = String::new();
+    io::stdin()
+        .read_line(&mut conversion_method)
+        .expect("Failed to read line");
+    println!("You choose convert method {}", conversion_method)
 }
