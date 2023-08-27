cd55a992309632d4686fe180350774112c856db0
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-27 15:07:17 +0530

    add descriptive string to print to inform the user.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to Fahrenheit, Celsius Converter....");
+    println!("What would you like to convert from and to?");
+    println!("1. Fahrenheit to Celsius\n2. Celsius to Fahrenheit");
 }
