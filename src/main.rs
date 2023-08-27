20bd96bf472ee513b8fb8525bd2ab46206971c66
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-27 15:32:33 +0530

    add control flow methods in relation to the input.

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,17 @@ fn main() {
     println!("What would you like to convert from and to?");
     println!("1. Fahrenheit to Celsius\n2. Celsius to Fahrenheit");
     // choose conversion method input
-    let mut conversion_method = String::new();
+    let mut conversion_method_input = String::new();
     io::stdin()
-        .read_line(&mut conversion_method)
+        .read_line(&mut conversion_method_input)
         .expect("Failed to read line");
-    println!("You choose convert method {}", conversion_method)
+    let mut conversion_method:&str;
+    if conversion_method_input =="1" {
+        conversion_method = "Fahrenheit to Celsius";
+    } else if conversion_method_input == "2" {
+        conversion_method = "Celsius to Fahrenheit";
+    } else {
+        conversion_method = "else";
+    }
+    println!("You choose convert method {}-{}", conversion_method, conversion_method_input)
 }
